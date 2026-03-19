959a28aed02b3a08d33e0da40cc98391d5f26b2b
Miguel Rodrigues <miguel4rodrigues@gmail.com>
2026-03-19 16:55:06 +0000

    Add Classes

--- DungeonStats/Program.cs
+++ DungeonStats/Program.cs
@@ -6,7 +6,35 @@ namespace DungeonStats
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            int request1 = int.Parse(args[0]);
+            int request2 = int.Parse(args[1]);
+            
+            Console.WriteLine(Damage(request1, request2));
+            Console.WriteLine(Damage(request1));
+            Console.WriteLine(CriticalHit(request1));
+        }
+
+        private static int Damage (int attack, int defense)
+        {
+            int damage = attack - defense;
+            return damage;
+        }
+
+        private static int Damage (int attack)
+        {
+            int damage = attack;
+            return damage;
+        }
+
+        private static int CriticalHit (int damage)
+        {
+            int crit;
+            if (damage == 1)
+                crit = 1;
+            else
+                crit = damage + CriticalHit(damage + (damage - 1));
+            return crit;
+            
         }
     }
 }
